66379a97ef4cd37d637f22881dc89ca75a000255
Precious <precious.akam@hotmail.com>
2022-09-25 11:47:35 +0930

    Changes

--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,11 @@
 #import modules
 import os
+import secrets
 from os import path
 from flask import Flask, abort, Blueprint, render_template, request, flash, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, login_user, login_required, current_user, logout_user, UserMixin
+from flask_wtf.file import FileField, FileRequired, FileAllowed
 from sqlalchemy.sql import func, select
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
@@ -17,34 +19,68 @@ db = SQLAlchemy(app)
 views = Blueprint('views', __name__)
 mail = Mail(app)
 
+from .models import User, Files, Survey
+from .forms import StartUpForm, UploadForm, EditProfileForm,  StartUpForm
+from . import mail
 
 def create_app():
     #app = Flask(__name__)
     
     #db = SQLAlchemy()
+    basedir = os.path.abspath(os.path.dirname(__file__))
     
     Authe = "authe.db"
     #secure cookies data
     app.config['SECRET_KEY'] = 'lolo'
-    app.config ["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///tmp/authe.db'
+    app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'authe.db')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    from .models import User
+    #set up admin
+    admin = Admin(app, name='control panel', template_mode='bootstrap3')
+    #initialise database
+    #db.init_app(app)
 
+    #Create Class controller * change this
+    class Controller(ModelView):
+        def is_accessible(self):
+            if current_user.is_admin == True:
+                return current_user.is_authenticated
+            else:
+                return abort(404)
+            #return current_user.is_authenticated
+        def not_auth(self):
+            return "You are not authorised to view this page"
+
+            
+    #create admin view
+    admin.add_view(Controller(User, db.session))
+    
+    #Set up loginmanager
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = "login"
+    login_manager.login_message_category = "info"
+
+    
     #import routes
     from .views import views
     from .auth import auth
 
+    #def create_database(app)
+    if not path.exists('/' + Authe):
+            db.create_all(app=app)
+            print('Database created')
+
     #set up email
-    app.config['MAIL_SERVER']='smtp.gmail.com'
+    app.config['MAIL_SERVER']='smtp.gmail.com' #127.0.0.1
     app.config['MAIL_PORT'] = 465
     app.config['MAIL_USERNAME'] = None
     app.config['MAIL_PASSWORD'] = None
     app.config['MAIL_USE_TLS'] = False
-    app.config['MAIL_USE_SSL'] = True
+    app.config['MAIL_USE_SSL'] = False
     #mail = Mail(app)
 
-    #set up admin
-    admin = Admin(app, name='control panel', template_mode='bootstrap3')
+    
 
     #initialise database
     # db.init_app(app)
@@ -53,33 +89,9 @@ def create_app():
     app.register_blueprint(views, url_prefix = "/")
     app.register_blueprint(auth, url_prefix = "/")
 
-    #Set up loginmanager
-    login_manager = LoginManager()
-    login_manager.init_app(app)
-    login_manager.login_view = "login"
-
-    #from . import models
-    User()
-
-    #Create Class controller
-    class Controller(ModelView):
-        def is_accessible(self):
-            if current_user.is_admin == True:
-                return current_user.is_authenticated
-            else:
-                return abort(404)
-            return current_user.is_authenticated
-        def not_auth(self):
-            return "You are not authorised to view this page"
-
-    #create admin view
-    admin.add_view(Controller(User, db.session))
     
-    #def create_database(app)
-    
-    if not path.exists('/' + Authe):
-            db.create_all(app=app)
-            print('Database created')
+
+
 
     #Create User Loader
     #login_manager = LoginManager()
--- website/auth.py
+++ website/auth.py
@@ -18,55 +18,72 @@ from .models import db, User
 #define login
 @auth.route('/login', methods = ['GET','POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('views.profile'))
     form = LoginForm()
-    if request.method == 'POST':
+    if request.method == 'POST' and form.validate_on_submit():
         email = form.email.data
         password = form.password.data
         user = User.query.filter_by(email=email).first()
         if user:
             if user.password == password:
-                login_user(user)
-                return redirect(url_for('profile'))      
+                login_user(user, remember = form.remember.data)
+                return redirect(url_for('views.profile'))      
     return render_template('login.html', form = form)
 
 #define logout
 @auth.route('/logout')
 def logout():
     logout_user()
-    return render_template('home.html')
+    return "you have logged out Click here to go back to the <a href='/'>home page</a>"
 
 
 #define sign up
 @auth.route("/sign-up", methods = ['GET','POST'])
 def sign_up():
     #Get submissions from signup post
-    # if request.method == 'POST':
+    if current_user.is_authenticated:
+        return redirect(url_for('views.profile'))
     form = RegistrationForm()
-    if form.validate_on_submit():
-        if len(form.email.data) < 4:
-            flash ("Email must be more than 4 characters long", category = 'error')
-        elif len(form.username.data) < 2:
-            flash ('first name must be more than 2 characters long', category = 'error')
-        elif form.password.data != form.confirm_password.data:
-            flash ('password must be the same', category = 'error')
-        elif len(form.password.data) < 6:
-            flash ('password cannot be less than 6 characters', category = 'error')
-        else:
-            #add user to database
-            new_user = User(name = form.username.data, email = form.email.data, password1 = form.password.data , password2 = form.confirm_password.data )
-            db.session.add(new_user)
-            db.session.commit()
-            login_user(new_user, remember=True)
-            flash ('Account created', category = 'success')
-            return redirect(url_for('views.home'))
+    if request.method == 'POST' and form.validate_on_submit():
+        new_user = User(name = form.username.data, email = form.email.data, password = form.password.data )
+        db.session.add(new_user)
+        db.session.commit()
+        login_user(new_user, remember=True)
+        flash ('Account created', category = 'success')
+        return redirect(url_for('views.profile'))
 
     return render_template('sign_up.html', form = form)
 
 #define admin sign up route
 @auth.route("/admin-sign-up", methods = ['GET','POST'])
 def admin_sign_up():
-    from .models import User
-    if request.method =='POST':
-        new_user = User(email = request.form['email'], password = request.form['password'] )
+    form = RegistrationForm()
+    if form.validate_on_submit and request.method == 'POST':
+        new_user = User(email = form.email.data, password = form.password.data, name = form.username.data, admin = True)
+        current_user.is_admin = True
+        db.session.add(new_user)
+        db.session.commit()
+        login_user(new_user, remember=True)
+        flash ('Account created', category = 'success')
+        
     return render_template('admin_sign_up.html')
+
+# #define admin login route * remember to add more to this
+@auth.route("/admin-login", methods = ['GET','POST'])
+def admin_login():
+    form = LoginForm()
+    if request.method =='POST':
+        email = form.email.data
+        password = form.password.data
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if user.password == password:
+                login_user(user)
+                return redirect(url_for('views.admin'))
+    return render_template('admin_login.html')
+
+
+
+
 
